9f09b81d1bf8229a5b781c73942d2feb7cda13c6
Chang Xiao <chang.x1994@gmail.com>
2025-01-30 19:44:19 -0500

    generalize compile script

--- compile.bat
+++ compile.bat
@@ -1,3 +1,5 @@
-C:\Programs\VulkanSDK_1_4_304\Bin\glslc.exe shaders\test.vert -o shaders\compiled\SPIRV\test.vert.spv
-C:\Programs\VulkanSDK_1_4_304\Bin\glslc.exe shaders\test.frag -o shaders\compiled\SPIRV\test.frag.spv
+@echo off
+
+@REM compile all shader files into SPIRV binary and store in SPIRV folder
+for %%i in (shaders\*) do echo Compiling %%i to shaders\SPIRV\%%~nxi & C:\Programs\VulkanSDK_1_4_304\Bin\glslc.exe %%i -o shaders\SPIRV\%%~nxi.spv
 pause
