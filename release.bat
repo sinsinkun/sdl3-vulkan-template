21a9add74c6a144690627029cf4e26b7db2516ea
Chang Xiao <chang.x1994@gmail.com>
2025-02-05 00:31:08 -0500

    better devex

--- release.bat
+++ release.bat
@@ -1,2 +1,4 @@
 @REM Compile standalone exe with SDL3.dll dependency
-g++ -O2 -o build\release.exe -g **.cpp src\**.cpp -std=c++17 -IC:\Programs\SDL3\include -LC:\Programs\SDL3\lib -DPLATFORM_DESKTOP -lsdl3 -static -lgdi32 -lwinmm -mwindows
+g++ -O2 -o build\release.exe -g **.cpp src\**.cpp -std=c++17 ^
+-IC:\Programs\SDL3\include -LC:\Programs\SDL3\lib ^
+-DPLATFORM_DESKTOP -lsdl3 -static -lgdi32 -lwinmm -mwindows
